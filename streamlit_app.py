26d640ed48dd67b259901fe88fdc18a5ef065f27
Nassim AKK <59050605+simsimdz@users.noreply.github.com>
2023-05-11 16:32:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit 
 
 streamlit.title('My parrent New Healthy Dinner')
+ 
 
-
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
